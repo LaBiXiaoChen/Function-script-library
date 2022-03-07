5b9897e9ee8af61a0e162dd3a67556fbc7da4dcb
xiaochen <2368923027@qq.com>
2022-03-07 09:23:12 +0800

    add and change

---
+++ shell脚本/aliasMyMac.sh
@@ -0,0 +1,15 @@
+#用于设置我的mac常用的命令别名，带有明显个人习惯，不方便分享，比较有较大的使用限制
+# TODO:遇到一个小bug,我想要让别名命令生效的时候,一直显示找不到.zhsrc文件
+# 一直在报这个错误
+# sh: /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh: No such file or directory
+
+#输出默认shell
+echo $SHELL
+
+#输出默认的环境变量
+echo $PATH
+
+#设置ls命令
+alias ll='ls -l'
+
+source .bashrc
---
+++ shell脚本/mas一键升级mac应用.sh
@@ -0,0 +1,9 @@
+#通常我们更新macOS的应用都是通过Mac App Store来更新，但对于不想打开 Store 的人来说 mas 是一个不错的选择，可以通过命令行方便的更新应用。
+
+# 安装 mas 的前提是拥有 brew 的环境
+
+# 安装mas
+brew install mas
+
+# 一键升级所有的Store应用
+mas upgrade
