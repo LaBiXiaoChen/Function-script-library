1c2e3fc3b7aafbfd1cbaea41a9f711c58e753398
xiaochen <2368923027@qq.com>
2022-03-07 19:28:16 +0800

    add

--- shell脚本/aliasMyMac.sh
+++ shell脚本/aliasMyMac.sh
@@ -1,15 +1,19 @@
 #用于设置我的mac常用的命令别名，带有明显个人习惯，不方便分享，比较有较大的使用限制
-# TODO:遇到一个小bug,我想要让别名命令生效的时候,一直显示找不到.zhsrc文件
-# 一直在报这个错误
-# sh: /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh: No such file or directory
 
 #输出默认shell
+echo '正在输出默认shell'
 echo $SHELL
 
 #输出默认的环境变量
+echo '输出默认的环境变量'
 echo $PATH
 
-#设置ls命令
-alias ll='ls -l'
+# 创建~/.zshrc_profile
+sudo touch ~/.zshrc_profile
 
-source .bashrc
+#设置ls命令
+echo "已设置alias ll='ls -la'"
+sudo echo "alias ll='ls -la'" > ~/.zshrc_profile
+
+# 启用zshrc_profile文件
+source ~/.zshrc_profile
---
+++ shell脚本/zshUseBash启用bashrc文件.sh
@@ -0,0 +1,3 @@
+# zsh想要使用部分bash命令必须启用bashrc才可以,比如source,zsh不自带
+echo 'zsh已启用bashrc,部分bash命令,zsh也可使用,请放心使用'
+source ~/.bash_profile
