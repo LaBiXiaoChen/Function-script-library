b8b41288687dbf8fb190254e7e7d7285275175a6
labixiaochen <2368923027@qq.com>
2022-03-10 15:15:49 +0800

    vim配置脚本

---
+++ shell脚本/vim脚本配置.sh
@@ -0,0 +1,9 @@
+# 安装终极vim配置
+git clone https://git.oschina.net/eccozhou/vimrc.git ~/.vim_runtime
+
+# 执行以下命令
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+echo '如果你对vim终极配置不是很了解的话，请访问下方链接：https://gitee.com/LaBiXiaoChen7/vimrc'
+echo 'https://gitee.com/LaBiXiaoChen7/vimrc'
+echo '此链接永远有效！！！'
