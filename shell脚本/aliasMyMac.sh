b44379a296927e1f7f59458479dae567867a380a
labixiaochen <2368923027@qq.com>
2022-03-10 21:10:37 +0800

    完成起别名脚本fish文件配置

--- shell脚本/aliasMyMac.sh
+++ shell脚本/aliasMyMac.sh
@@ -1,6 +1,9 @@
-#用于设置我的mac常用的命令别名，带有明显个人习惯，不方便分享，比较有较大的使用限制，本人默认使用fish shell进行操作
+#用于设置我的mac常用的命令别名，本人默认使用fish shell进行操作
 
-# TODO：未设置fish shell的shell环境情况
+echo '此脚本只对fish进行操作，下面将会对你的mac安装fish shell'
+brew install fish
+echo 'fish shell 已经安装成功'
+
 #输出默认shell
 echo '正在输出默认shell'
 echo $SHELL
@@ -9,14 +12,13 @@ echo $SHELL
 echo '输出默认的环境变量'
 echo $PATH
 
-# 创建~/.zshrc_profile
-sudo touch ~/.zshrc_profile
-
 #设置ls命令
-echo "已设置alias ll='ls -la'"
-sudo echo "alias ll='ls -la'" > ~/.zshrc_profile
+echo "abbr -a l ls -al复制到文本 ~/.config/fish/config.fish"
+sudo echo "abbr -a ll 'ls -la'" >> ~/.config/fish/config.fish
 
-sudo echo "alias logo='neofetch'" > ~/.zshrc_profile
+echo "alias -a logo neofetch复制到文本 ~/.config/fish/config.fish"
+sudo echo "abbr -a logo neofetch" >> ~/.config/fish/config.fish
 
-# 启用zshrc_profile文件
-source ~/.zshrc_profile
+# 启动配置到环境中
+echo '启动配置到环境中'
+source ~/.config/fish/config.fish
