43daf8a94ce4eb713b146857c4838daf72af6948
labixiaochen <2368923027@qq.com>
2022-03-11 14:40:23 +0800

    change

--- shell脚本/start所有脚本的钩子.sh
+++ shell脚本/start所有脚本的钩子.sh
@@ -9,6 +9,14 @@
 echo '欢迎您来到我的bash脚本系统中'
 # say 'welcome my dear sir,enjoy'
 
+# TODO：前面需要安装fish shell
+
+echo '下载fish shell'
+brew install fish
+
+echo '切换到fish shell中进行操作'
+fish
+
 # 前期环境搭建
 # 安装常用必备的工具
 # wget安装
@@ -21,7 +29,11 @@ brew install neofetch
 brew install fish
 # sl安装，一个会跑的火车
 brew install sl
+# Axel多线程下载工具安装
+brew install axel
+# 安装mas
+brew install mas
+
 
 echo '现在在给我的脚本们提升权限'
-# TODO:脚本批量提升权限问题还未解决,钩子脚本未完成
-chmod 755 -R ./*
+# TODO:脚本批量提升权限问题还未解决,钩子脚本未完成,如果解决不了，我们可以换一个思路
